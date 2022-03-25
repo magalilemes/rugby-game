3f0a06316cc296f2b44d8e14d201bc4fb953cc7c
Magali Lemes <magalilemes00@gmail.com>
2022-03-25 12:14:58 -0300

    Ataque sem usar Spy

--- src/attacker.c
+++ src/attacker.c
@@ -19,11 +19,28 @@
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
   UNUSED(defender_spy);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  int i;
+
+  static direction_t directions_priorities[] = {DIR_UP_RIGHT, DIR_UP, DIR_RIGHT,
+  DIR_DOWN_RIGHT, DIR_DOWN, DIR_UP_LEFT, DIR_DOWN_LEFT, DIR_LEFT};
+
+  static position_t previous_position = { .i = 0, .j = 0};
+
+  /* Se na rodada anterior, o atacante tentou se mexer, mas não conseguiu */
+  if (previous_position.i == attacker_position.i && previous_position.j == attacker_position.j){
+
+    /* Modifica a prioridade das direções em 1 para esquerda */
+    direction_t tmp = directions_priorities[0];
+    for (i = 0; i < 7; i++)
+      directions_priorities[i] = directions_priorities[i + 1];
+    directions_priorities[7] = tmp;
+  }
+
+  previous_position = attacker_position;
+
+  return directions_priorities[0];
 }
 
 /*----------------------------------------------------------------------------*/
