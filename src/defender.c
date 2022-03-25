5b594abac0c3cba9f9ee3ebc487243e8133b8e03
Magali Lemes <magalilemes00@gmail.com>
2022-03-25 18:51:40 -0300

    Defesa sem usar Spy

--- src/defender.c
+++ src/defender.c
@@ -19,11 +19,28 @@
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
   UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  int i;
+
+  static direction_t directions_priorities[] = {DIR_UP_LEFT, DIR_DOWN_LEFT,
+  DIR_LEFT, DIR_UP, DIR_DOWN, DIR_UP_RIGHT, DIR_DOWN_RIGHT, DIR_RIGHT};
+
+  static position_t previous_position = { .i = 0, .j = 0};
+
+  /* Se na rodada anterior, o defensor tentou se mexer, mas não conseguiu */
+  if (previous_position.i == defender_position.i && previous_position.j == defender_position.j){
+
+    /* Modifica a prioridade das direções em 1 para esquerda */
+    direction_t tmp = directions_priorities[0];
+    for (i = 0; i < 7; i++)
+      directions_priorities[i] = directions_priorities[i + 1];
+    directions_priorities[7] = tmp;
+  }
+
+  previous_position = defender_position;
+
+  return directions_priorities[0];;
 }
 
 /*----------------------------------------------------------------------------*/
